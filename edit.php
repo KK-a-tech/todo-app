0fbfb111cd8164c21432fc26e6d8b471a4c630cf
KK-a-tech <kento.k711@gmail.com>
2024-09-29 23:31:24 +0900

    update:edit.php表示の際matrix_idに初期値を付与する機能を追加

--- edit.php
+++ edit.php
@@ -15,16 +15,54 @@ $todo = getSelectedTodo($_GET['id']);
   <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-  helloWorld
   <form action="store.php" method="post">
     <input type="hidden" name="id" value="<?= $_GET['id']; ?>">
     <input type="text" name="content" value="<?= $todo['content'] ?>">
-    <select name="matrix id">
-      <option value='1'>第1象限</option>
-      <option value="2">第2象限</option>
-      <option value="3">第3象限</option>
-      <option value="4">第4象限</option>
-    </select>
+    <!-- matrix_idの初期値を設定 -->
+    <?php
+    switch ($todo['matrix_id']) {
+        case '1':
+            echo '<select name="matrix_id">
+                <option value="1" selected>第1象限</option>
+                <option value="2">第2象限</option>
+                <option value="3">第3象限</option>
+                <option value="4">第4象限</option>
+            </select>';
+            break;
+        case '2':
+            echo '<select name="matrix_id">
+                <option value="1">第1象限</option>
+                <option value="2" selected>第2象限</option>
+                <option value="3">第3象限</option>
+                <option value="4">第4象限</option>
+            </select>';
+            break;
+        case '3':
+            echo '<select name="matrix_id">
+                <option value="1">第1象限</option>
+                <option value="2">第2象限</option>
+                <option value="3" selected>第3象限</option>
+                <option value="4">第4象限</option>
+            </select>';
+            break;
+        case '4':
+            echo '<select name="matrix_id">
+                <option value="1">第1象限</option>
+                <option value="2">第2象限</option>
+                <option value="3">第3象限</option>
+                <option value="4" selected>第4象限</option>
+            </select>';
+            break;
+        default:
+            echo '<select name="matrix_id">
+                <option value="1">第1象限</option>
+                <option value="2">第2象限</option>
+                <option value="3">第3象限</option>
+                <option value="4">第4象限</option>
+            </select>';
+            break;
+    }
+    ?>
     <input type="submit" value="更新">
   </form>
   <div>
